fa2fd1653bb3e7c04a3bd3a3ea8846dbfc1aacec
Chase Buhler <cmbuhler@ualberta.ca>
2020-01-30 22:11:49 -0700

    Commit to merge

--- BasePersistentLogger.hpp
+++ BasePersistentLogger.hpp
@@ -6,15 +6,26 @@ class BasePersistentLogger {
 protected:
     std::string _databaseIP;
     std::string _databasePort;
+    std::string _databaseUsername;
+    std::string _databasePassword;
+    std::string _databaseName;
 
     virtual void connect() = 0;
     virtual void disconnect() = 0;
 public:
     BasePersistentLogger(){
         _databaseIP = "127.0.0.1";
-        _databasePort = "27017";
+        _databasePort = "";
     }
-    BasePersistentLogger(std::string databaseIP, std::string databasePort){
+    BasePersistentLogger(std::string databaseIP, std::string databasePort, std::string databaseName){
+        _databaseIP = databaseIP;
+        _databasePort = databasePort;
+        _databaseName = databaseName;
+        _databaseUsername = nullptr;
+        _databasePassword = nullptr;
+    }
+    BasePersistentLogger(std::string databaseIP, std::string databasePort, std::string databaseName,
+            std::string databaseUsername, std::string databasePassword){
         _databaseIP = databaseIP;
         _databasePort = databasePort;
     }
