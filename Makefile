086258bd7974f523f21eac7288f4f07ef4c657c1
Chase Buhler <cmbuhler@ualberta.ca>
2020-02-07 11:07:12 -0700

    Update Test.cpp to conditionally include Cassandra or Mongo (Makefile still needs work)

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@ SOURCES = $(wildcard *.cpp)
 OBJECTS = $(SOURCES:%.cpp=%.o)
 
 
-all: clean compile cassandra mongo
+all: clean compile
 clean:
 	rm -f *.o cassandra mongo
 compile:
 %.o: %.cpp
-	$(CC) ${CFLAGS} -c $^ -o $@ -I/usr/local/include/mongocxx/v_noabi -I/usr/local/include/bsoncxx/v_noabi
+	$(CC) ${CFLAGS} -c $^ -o $@ -I/usr/local/include/mongocxx/v_noabi -I/usr/local/include/bsoncxx/v_noabi -DCASSANDRA_LOGGER
 
 cassandra: $(OBJECTS)
-	$(CC) -o cassandra Test.o CassandraLogger.o -Lcpp-build/build -lcassandra
+	$(CC) -o test.out Test.o CassandraLogger.o -Lcpp-build/build -lcassandra -DCASSANDRA_LOGGER
 
 mongo: $(OBJECTS)
-	$(CC) -o mongo test.o MongoLogger.o -L/usr/local/lib -lmongocxx -lbsoncxx
+	$(CC) -o test.out Test.o MongoLogger.o -L/usr/local/lib -lmongocxx -lbsoncxx -DMONGO_LOGGER
 
--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,24 @@
+#include "BasePersistentLogger.hpp"
+
+#if defined CASSANDRA_LOGGER
 #include "CassandraLogger.hpp"
+#define DEFAULT_PORT "9042"
+typedef CassandraLogger PersistentLogger;
+
+#elif defined MONGO_LOGGER
+#include "MongoLogger.hpp"
+#define DEFAULT_PORT "27017"
+typedef MongoLogger PersistentLogger;
+
+#else
+#error Please specify one of compile variables MONGO_LOGGER CASSANDRA_LOGGER to enable Persistent Logging
+#endif
+
 int main() {
     
-    CassandraLogger* logger = new CassandraLogger();
+    auto* logger = new PersistentLogger("127.0.0.1", DEFAULT_PORT, "jitserver_logs", "jitlogger", "jitlogger");
     logger->connect();
-    string logs =  "\
+    std::string logs =  "\
     0x7f38af800194 00000134 [0x7f38c13565f0] 48 83 7d 50 ff \
     cmpqword ptr [rbp+0x50], 0xffffffffffffffff	# CMP8MemImms, \
     SymRef  stackOverflowMark[#455  MethodMeta +80] [flags 0x200 0x0 ]\
@@ -11,8 +26,8 @@ int main() {
     0x7f38af800199 00000139 [0x7f38c1380590]                                    vfpSave			# AdjustFramePtr\
     0x7f38af800199 00000139 [0x7f38c13567e0] 0f 84 d1 01 00 00                  je	Outlined Label L0080			# JE4\
     0x7f38af80019f 0000013f [0x7f38c1356bd0]  Label L0082:			# LABEL	# (End of internal control flow)";
-    string clientID = "570241675495946978";
-    string method = "java/util/Hashtable.put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;";
+    std::string clientID = "570241675495946978";
+    std::string method = "java/util/Hashtable()";
     logger->logMethod(method, clientID, logs);
     logger->disconnect();
     logger->connect();
