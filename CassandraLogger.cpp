a43c715a4fc6807f474df59ad32fa5fa5704161e
XuechunHou <xhou1@ualberta.ca>
2020-02-07 23:25:10 -0700

    removed default constructor in BaseLogger

--- CassandraLogger.cpp
+++ CassandraLogger.cpp
@@ -1,18 +1,11 @@
 #include <time.h>
 #include <iostream>
-#include <cassandra.h>
+
 #include "CassandraLogger.hpp"
 CassandraLogger::CassandraLogger(std::string const &databaseIP, std::string const &databasePort, std::string const &databaseName): BasePersistentLogger(databaseIP, databasePort, databaseName){
     _session = NULL;
     _connectFuture = NULL;
     _cluster = NULL;
-}
-
-CassandraLogger::CassandraLogger(): BasePersistentLogger(){
-    _session = NULL;
-    _connectFuture = NULL;
-    _cluster = NULL;
-    _databasePort = "9042";
 }
 
 CassandraLogger::CassandraLogger(std::string const &databaseIP, std::string const &databasePort,
@@ -23,8 +16,8 @@ CassandraLogger::CassandraLogger(std::string const &databaseIP, std::string cons
     _cluster = NULL;
 }
 
-bool CassandraLogger::createKeySpace(std::string const &keyspace) {
-    string queryString = "CREATE KEYSPACE IF NOT EXISTS " + keyspace + " WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1};";
+bool CassandraLogger::createKeySpace() {
+    std::string queryString = "CREATE KEYSPACE IF NOT EXISTS " +  _databaseName + " WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1};";
     CassStatement* statement = cass_statement_new(queryString.c_str(), 0);
 
     CassFuture* queryFuture = cass_session_execute(_session, statement);
@@ -44,9 +37,9 @@ bool CassandraLogger::createKeySpace(std::string const &keyspace) {
     return true;
 
 }
-bool CassandraLogger::createTable(std::string const &keyspace, std::string const &tableName) {
+bool CassandraLogger::createTable(std::string const &tableName) {
    
-    string queryString = "CREATE TABLE IF NOT EXISTS " + keyspace + "." + tableName + " (clientID text, methodName text, logContent text, insertionDate date,insertionTime time, primary key (clientID, methodName, insertionDate, insertionTime));";
+    std::string queryString = "CREATE TABLE IF NOT EXISTS " + _databaseName + "." + tableName + " (clientID text, methodName text, logContent text, insertionDate date,insertionTime time, primary key (clientID, methodName));";
     CassStatement* statement = cass_statement_new(queryString.c_str(), 0);
 
     CassFuture* queryFuture = cass_session_execute(_session, statement);
@@ -78,7 +71,7 @@ bool CassandraLogger::connect() {
     /*Set protocol version */
     CassError rc_set_protocol = cass_cluster_set_protocol_version(_cluster, 4);
     if (rc_set_protocol != CASS_OK) {
-        cout << cass_error_desc(rc_set_protocol) << endl;
+        std::cout << cass_error_desc(rc_set_protocol) << std::endl;
         return false;
 
     }
@@ -86,7 +79,7 @@ bool CassandraLogger::connect() {
     /* Add contact points */
     CassError rc_set_ip = cass_cluster_set_contact_points(_cluster, _databaseIP.c_str());
     if (rc_set_ip != CASS_OK) {
-        cout << cass_error_desc(rc_set_ip) << endl;
+       std::cout << cass_error_desc(rc_set_ip) << std::endl;
         return false;
 
     }
@@ -94,7 +87,7 @@ bool CassandraLogger::connect() {
     /*Set port number*/
     CassError rc_set_port = cass_cluster_set_port(_cluster, stoi(_databasePort));
     if (rc_set_port != CASS_OK) {
-        cout << cass_error_desc(rc_set_port) << endl;
+        std::cout << cass_error_desc(rc_set_port) << std::endl;
         return false;
 
     }
@@ -118,31 +111,31 @@ bool CassandraLogger::connect() {
 bool CassandraLogger::logMethod(std::string const &method, std::string const &clientID, std::string const &logContent){
     
     // create table space and table first
-    if (!createKeySpace("test")) return false;
-    if (!createTable("test","logs")) return false;
+    if (!createKeySpace()) return false;
+    if (!createTable("logs")) return false;
 
-    
+    std::string queryString = "INSERT INTO " + _databaseName + ".logs (clientID, methodName, logContent, insertionDate, insertionTime) VALUES (?, ?, ?, ?, ?)";
     CassStatement* statement
-    = cass_statement_new("INSERT INTO test.logs (clientID, methodName, logContent, insertionDate, insertionTime) VALUES (?, ?, ?, ?, ?)", 5);
+    = cass_statement_new(queryString.c_str(), 5);
 
     /* Bind the values using the indices of the bind variables */
     CassError rc_set_bind_pk = cass_statement_bind_string(statement, 0, clientID.c_str());
     if (rc_set_bind_pk != CASS_OK) {
-        cout << cass_error_desc(rc_set_bind_pk) << endl;
+        std::cout << cass_error_desc(rc_set_bind_pk) << std::endl;
         cass_statement_free(statement);
         return false;
     }
 
     CassError rc_set_bind_method = cass_statement_bind_string(statement, 1, method.c_str());
     if (rc_set_bind_pk != CASS_OK) {
-        cout << cass_error_desc(rc_set_bind_method) << endl;
+        std::cout << cass_error_desc(rc_set_bind_method) << std::endl;
         cass_statement_free(statement);
         return false;
     }
     CassError rc_set_bind_log_content = cass_statement_bind_string(statement, 2, logContent.c_str());
 
      if (rc_set_bind_log_content != CASS_OK) {
-        cout << cass_error_desc(rc_set_bind_log_content) << endl;
+        std::cout << cass_error_desc(rc_set_bind_log_content) << std::endl;
         cass_statement_free(statement);
         return false;
     }
@@ -157,14 +150,14 @@ bool CassandraLogger::logMethod(std::string const &method, std::string const &cl
     CassError rc_set_bind_insertion_date = cass_statement_bind_uint32(statement, 3, year_month_day_of_insertion);
     
     if (rc_set_bind_insertion_date != CASS_OK) {
-        cout << cass_error_desc(rc_set_bind_insertion_date) << endl;
+        std::cout << cass_error_desc(rc_set_bind_insertion_date) << std::endl;
         cass_statement_free(statement);
         return false;
     }
     /* 'time' uses a signed 64-bit integer */
     CassError rc_set_bind_insertion_time = cass_statement_bind_int64(statement, 4, time_of_insertion);
     if (rc_set_bind_insertion_time != CASS_OK) {
-        cout << cass_error_desc(rc_set_bind_insertion_time) << endl;
+        std::cout << cass_error_desc(rc_set_bind_insertion_time) << std::endl;
         cass_statement_free(statement);
         return false;
     }
