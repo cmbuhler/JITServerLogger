4fdefa4014037f2d6f4551b6a8ea20de0a925a27
Chase Buhler <cmbuhler@ualberta.ca>
2020-02-10 17:19:07 -0700

    Test build with cmake

---
+++ build/CMakeFiles/mongo_test.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mongo_test.out.dir/Test.cpp.o"
+  "CMakeFiles/mongo_test.out.dir/MongoLogger.cpp.o"
+  "mongo_test.out.pdb"
+  "mongo_test.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mongo_test.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
